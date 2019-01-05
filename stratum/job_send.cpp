67ac80e532222437687b2b8fbda6c24b5db19532
GenieG <allMN-dev@protonmail.com>
2019-01-05 21:01:31 +0300

    Rel v1

--- stratum/job_send.cpp
+++ stratum/job_send.cpp
@@ -20,8 +20,7 @@ static void job_mining_notify_buffer(YAAMP_JOB *job, char *buffer)
 		debuglog("Log2: {\"id\":null,\"method\":\"mining.notify\",\"params\":[\"%x\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",[%s],\"%s\",\"%s\",\"%s\",\"%s\",true]}\n",
 			job->id, templ->prevhash_be, templ->denom10, templ->denom100, templ->denom1000, templ->denom10000, templ->prooffullnode, templ->coinb1, templ->coinb2, templ->txmerkles, templ->version, templ->nbits, templ->ntime);
 
-		sprintf(buffer, "{\"id\":null,\"method\":\"mining.notify\",\"params\":["
-			"\"%x\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",[%s],\",\"%s\",\"%s\",\"%s\",true]}\n",
+		sprintf(buffer, "{\"id\":null,\"method\":\"mining.notify\",\"params\":[\"%x\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",\"%s\",[%s],\"%s\",\"%s\",\"%s\",\"%s\",true]}\n",
 			job->id, templ->prevhash_be, templ->denom10, templ->denom100, templ->denom1000, templ->denom10000, templ->prooffullnode, templ->coinb1, templ->coinb2, templ->txmerkles, templ->version, templ->nbits, templ->ntime);
 		return;
 	} else if (!strcmp(g_stratum_algo, "lbry")) {
